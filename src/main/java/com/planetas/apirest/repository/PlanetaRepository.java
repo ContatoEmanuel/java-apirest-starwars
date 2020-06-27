979efef3412707e3bb2ef8b22e5ec5a445a98c51
Emanuel Arrudas de Macêdo <contato.emanuel97@gmail.com>
2020-06-27 12:38:53 -0300

    adicionar e listar planetas

--- src/main/java/com/planetas/apirest/repository/PlanetaRepository.java
+++ src/main/java/com/planetas/apirest/repository/PlanetaRepository.java
@@ -1,5 +1,14 @@
 package com.planetas.apirest.repository;
 
-public interface PlanetaRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.planetas.apirest.models.Planeta;
+
+public interface PlanetaRepository extends JpaRepository<Planeta, Long>{
+
+	List<Planeta> findAll();
+	Planeta findById(long id);
+	Planeta findByNome(String nome);
 }
---
+++ src/main/java/com/planetas/apirest/resources/PlanetaResource.java
@@ -0,0 +1,43 @@
+package com.planetas.apirest.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.planetas.apirest.models.Planeta;
+import com.planetas.apirest.repository.PlanetaRepository;
+
+
+@RestController
+@RequestMapping(value="/api")
+public class PlanetaResource {
+
+		@Autowired
+		PlanetaRepository planetaRepository;
+		
+		@PostMapping("/planeta")
+		public Planeta adicionaPlaneta(@RequestBody Planeta planeta) {
+			return planetaRepository.save(planeta);
+		}
+		
+		@GetMapping("/planetas")
+		public List<Planeta> listaPlanetas(){
+			return planetaRepository.findAll();
+		}
+		
+		@GetMapping("/planeta/{nome}")
+		public Planeta buscaPlanetaNome(@PathVariable(value="nome")String nome){
+			return planetaRepository.findByNome(nome);
+		}
+		
+		@GetMapping("/planeta/id/{id}")
+		public Planeta buscaPlanetaId(@PathVariable(value="id")long id){
+			return planetaRepository.findById(id);
+		}
+}
