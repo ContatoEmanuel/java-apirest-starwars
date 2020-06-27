588fceeb0709af66a32c21c0295ee9e3e99fb362
Emanuel Arrudas de Macêdo <contato.emanuel97@gmail.com>
2020-06-27 13:24:11 -0300

    deletar e alterar planetas

--- src/main/java/com/planetas/apirest/resources/PlanetaResource.java
+++ src/main/java/com/planetas/apirest/resources/PlanetaResource.java
@@ -3,9 +3,11 @@ package com.planetas.apirest.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,5 +41,15 @@ public class PlanetaResource {
 		@GetMapping("/planeta/id/{id}")
 		public Planeta buscaPlanetaId(@PathVariable(value="id")long id){
 			return planetaRepository.findById(id);
+		}
+		
+		@DeleteMapping("/planeta")
+		public void removePlaneta(@RequestBody Planeta planeta) {
+			planetaRepository.delete(planeta);
+		}
+		
+		@PutMapping("/planeta")
+		public Planeta atualizaPlaneta(@RequestBody Planeta planeta) {
+			return planetaRepository.save(planeta);
 		}
 }
