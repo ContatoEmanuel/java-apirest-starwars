f894e6c3d368593f7c7b0cf151262e74962782b5
Emanuel Arrudas de Macêdo <contato.emanuel97@gmail.com>
2020-06-27 14:35:20 -0300

    adição do swagger

--- src/main/java/com/planetas/apirest/resources/PlanetaResource.java
+++ src/main/java/com/planetas/apirest/resources/PlanetaResource.java
@@ -3,6 +3,7 @@ package com.planetas.apirest.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,40 +16,51 @@ import org.springframework.web.bind.annotation.RestController;
 import com.planetas.apirest.models.Planeta;
 import com.planetas.apirest.repository.PlanetaRepository;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 
 @RestController
 @RequestMapping(value="/api")
+@Api(value="API REST Planetas")
+@CrossOrigin(origins="*")
 public class PlanetaResource {
 
 		@Autowired
 		PlanetaRepository planetaRepository;
 		
 		@PostMapping("/planeta")
+		@ApiOperation(value="Adiciona um Planeta.")
 		public Planeta adicionaPlaneta(@RequestBody Planeta planeta) {
 			return planetaRepository.save(planeta);
 		}
 		
 		@GetMapping("/planetas")
+		@ApiOperation(value="Retorna a Lista de Planetas.")
 		public List<Planeta> listaPlanetas(){
 			return planetaRepository.findAll();
 		}
 		
 		@GetMapping("/planeta/{nome}")
+		@ApiOperation(value="Acessa o Planeta pelo Nome.")
 		public Planeta buscaPlanetaNome(@PathVariable(value="nome")String nome){
 			return planetaRepository.findByNome(nome);
 		}
 		
 		@GetMapping("/planeta/id/{id}")
+		@ApiOperation(value="Acessa o Planeta pelo Id.")
 		public Planeta buscaPlanetaId(@PathVariable(value="id")long id){
 			return planetaRepository.findById(id);
 		}
 		
 		@DeleteMapping("/planeta")
+		@ApiOperation(value="Deleta um Planeta.")
 		public void removePlaneta(@RequestBody Planeta planeta) {
 			planetaRepository.delete(planeta);
 		}
 		
 		@PutMapping("/planeta")
+		@ApiOperation(value="Altera as Informações de um Planeta.")
 		public Planeta atualizaPlaneta(@RequestBody Planeta planeta) {
 			return planetaRepository.save(planeta);
 		}
